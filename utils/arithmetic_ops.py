9e48d01a4f8246be601a235f2076f012a54d9476
AdhamEbaidd <AdhamEbaid1@gmail.com>
2023-10-21 18:02:18 +0300

    added the squareWave , shiftWave , normalizeWave and accumulateWave functions to the arithmetic_ops.py file

--- utils/arithmetic_ops.py
+++ utils/arithmetic_ops.py
@@ -26,3 +26,29 @@ def multiplyWave(inputWave, factor):
     arr = np.array(inputWave)
     result = [element * factor for element in arr]
     return result
+
+def squarWave(inputWave):
+    arr = np.array(inputWave)
+    result = [pow(element,2) for element in arr]
+    return result
+
+def shiftWave(inputWave, value):
+    arr = np.array(inputWave)
+    result = [element +value for element in arr]
+    return result
+
+def normalizeWave(inputWave, choice):
+    arr = np.array(inputWave)
+    if choice == 0 : # from 0 to 1
+        normalized = (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+    elif choice == 1 : #from -1 to 1
+        normalized = (arr - np.min(arr)) / (np.max(arr) - np.min(arr)) * 2 - 1
+    else:
+        raise ValueError("Invalid choice.")
+
+    return normalized
+
+def accumulateWave(inputWave):
+    arr = np.array(inputWave)
+    accumulated = np.cumsum(arr)
+    return accumulated
