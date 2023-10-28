cf5d918cd1b97b6825211560e37f213376c6a135
ismail <ismaeelsherif2002@ymail.com>
2023-10-29 01:50:30 +0200

    Task 3 Done

--- dialogs/quantization_dialog.py
+++ dialogs/quantization_dialog.py
@@ -6,15 +6,6 @@ from testwave.Task3.Test2.QuanTest2 import QuantizationTest2
 import numpy as np
 import math as mt
 
-def show_output(arr1,arr2,arr3,arr4):
-    i=0
-    for i in range(len(arr1)) :
-        print(arr1[i],arr2[i],arr3[i],arr4[i])
-
-def show_output2(arr1,arr2):
-    i=0
-    for i in range(len(arr1)) :
-        print(arr1[i],arr2[i])
 
 def convert_to_binary(number, num_bits):
     binary = bin(number)[2:]  # Convert number to binary string
@@ -46,76 +37,88 @@ def openQuantizaDialog(root):
 
         if selected_type.get() == 'levels':
             number_of_levels = noOfBitsOrLevels
-            number_of_bits = int(mt.log2(noOfBitsOrLevels))
+            number_of_bits = int(mt.log2(number_of_levels))
             arr = np.array(magnitudes)
             maximum = max(arr)
             minimum = min(arr)
             delta = (maximum - minimum) / number_of_levels
-            ranges = np.arange(start=minimum, stop=maximum, step=delta)
+            ranges = [minimum]
+            for i in range(1, number_of_levels):
+                newVal = ranges[i-1] + delta
+                ranges.append(newVal)
+            ranges.append(maximum)
             ranges = [round(element , 2) for element in ranges]
+            
             quantized = []
             midpoints = []
-            x = round(minimum,2)
-            while x < maximum:
-                midpoint = (x+(x + delta))/2
-                midpoint = round(midpoint, 3)
+            
+            for i in range(0, len(ranges) - 1):
+                midpoint = (ranges[i+1] + ranges[i]) / 2
                 midpoints.append(midpoint)
-                print("midpoint for : ",x,"and ",x+delta,"is : ",midpoint)
-                x = round( x + delta , 2)
 
-            distances = []
             error = []
             which_interval = []
             which_interval_encoded = []
 
 
-            for i in range(len(arr)):
-                item = arr[i]
-                distances = [element - item  for element in midpoints]
-                min_dis = min(distances, key=abs)
-                error.append(round(min_dis, 3))
-                level = distances.index(min_dis)+1
-                quantized.append( midpoints[level-1])
+            for item in magnitudes:
+                levelIndex = 0
+                for i in range(0, len(ranges)):
+                    if ranges[i] >= item:
+                        if i > 0:
+                            levelIndex = i - 1
+                        else:
+                            levelIndex = 0
+                        break
+                level = levelIndex + 1
+
+                error.append( midpoints[levelIndex] - item )
+                quantized.append( midpoints[levelIndex])
                 which_interval.append(level)
-                which_interval_encoded.append(convert_to_binary(level-1 , number_of_bits))
-            print(len(which_interval) , len(which_interval_encoded) , len(quantized) , len(error))
-            show_output(which_interval , which_interval_encoded , quantized ,error)
+                which_interval_encoded.append(convert_to_binary(levelIndex , number_of_bits))
             return which_interval,which_interval_encoded,quantized,error
 
         else:  # bits
-            number_of_levels = mt.pow(2 , noOfBitsOrLevels)
             number_of_bits = noOfBitsOrLevels
+            number_of_levels = int(mt.pow(2 , number_of_bits))
+            
             arr = np.array(magnitudes)
             maximum = max(arr)
             minimum = min(arr)
             delta = (maximum - minimum) / number_of_levels
-            ranges = np.arange(start=minimum, stop=maximum, step=delta)
-            ranges = [round(element, 2) for element in ranges]
+            ranges = [minimum]
+            for i in range(1, number_of_levels):
+                newVal = ranges[i-1] + delta
+                ranges.append(newVal)
+            ranges.append(maximum)
+            ranges = [round(element , 2) for element in ranges]
+
             quantized = []
             midpoints = []
-            x = round(minimum, 2)
-            while x < maximum:
-                midpoint = (x + (x + delta)) / 2
-                midpoint = round(midpoint, 3)
+            
+            for i in range(0, len(ranges) - 1):
+                midpoint = (ranges[i+1] + ranges[i]) / 2
                 midpoints.append(midpoint)
-                print("midpoint for : ", x, "and ", x + delta, "is : ", midpoint)
-                x = round(x + delta, 2)
 
-            distances = []
             error = []
-            which_interval = []
             which_interval_encoded = []
 
-            for i in range(len(arr)):
-                item = arr[i]
-                distances = [element - item for element in midpoints]
-                min_dis = min(distances, key=abs)
-                error.append(round(min_dis, 3))
-                level = distances.index(min_dis) + 1
-                quantized.append(midpoints[level - 1])
-                which_interval.append(level)
-                which_interval_encoded.append(convert_to_binary(level - 1, number_of_bits))
-            show_output2(which_interval_encoded, quantized)
+            for item in magnitudes:
+
+                levelIndex = 0
+                for i in range(0, len(ranges)):
+                    if ranges[i] >= item:
+                        if i > 0:
+                            levelIndex = i - 1
+                        else:
+                            levelIndex = 0
+                        break
+                level = levelIndex + 1
+
+                error.append(item - midpoints[levelIndex])
+                quantized.append( midpoints[levelIndex])
+                which_interval_encoded.append(convert_to_binary(levelIndex , number_of_bits))
+
 
             return which_interval_encoded, quantized
 
@@ -125,11 +128,12 @@ def openQuantizaDialog(root):
         import sys
 
         rootPath = os.path.dirname(sys.argv[0])
+        
         if('1' in inputSignal.fileName):
-            filename = rootPath + '/testwave/Task3/Test1/Quan1_input.txt'
+            filename = rootPath + '/testwave/Task3/Test1/Quan1_Out.txt'
             QuantizationTest1(filename, result[0], result[1])
         elif('2' in inputSignal.fileName):
-            filename = rootPath + '/testwave/Task3/Test2/Quan2_input.txt'
+            filename = rootPath + '/testwave/Task3/Test2/Quan2_Out.txt'
             QuantizationTest2(filename, result[0], result[1], result[2], result[3])
 
     def show():
@@ -137,7 +141,8 @@ def openQuantizaDialog(root):
             result = calculateQuantizedSignal()
             if(result):
                 testResult(result)
-                wave_drawer.draw(inputSignal.x, result)
+                quantizedIndex = 1 if selected_type.get() == 'bits' else 2
+                wave_drawer.draw(inputSignal.x, result[quantizedIndex])
 
     
     dialog = tk.Toplevel(root)
